7ecb35a0944d09bce9ce982798d58b2c74a726ed
davidpls <davidzsigo.main@gmail.com>
2025-02-03 09:23:32 +0100

    Frontend_alap_1
    Frontend alapja

---
+++ Pollák Könyvklub/src/components/Card.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import { RouterLink } from "vue-router";
+
+
+
+const props = defineProps({
+    id: Number,
+    title: String,
+    director: String,
+    release_year: Number,
+    genre_1: String,
+    genre_2: String,
+    genre_3: String,
+    description: String,
+    movie_cover: String,
+});
+</script>
