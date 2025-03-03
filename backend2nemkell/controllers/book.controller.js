20673cc8a1c5c99f84565ab9a022338f98d45a7a
Csongor <162287506+SiposGellert@users.noreply.github.com>
2025-03-03 08:30:58 +0100

    backend2 törlés, frontend V2-n kívülre rakása

--- backend2nemkell/controllers/book.controller.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { listAllBooks } from "../services/actor.service.js";
-const router = express.Router();
-
-router.get("/getAll", async (req, res) => {
-  const data = await listAllBooks();
-  res.status(200).json(data);
-});
